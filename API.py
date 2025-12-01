2fb5337392b17a62345d9f17c083b6169329ed81
Youssef Mostafa <youssefm32@nycstudents.net>
2025-12-01 10:17:10 -0500

    Update API.py

--- API.py
+++ API.py
@@ -1,4 +1,4 @@
-import requests
+
 
 """ def getPoke(poke):
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
@@ -17,13 +17,17 @@ import requests
 pokemon = getPoke("Bulbasaur")
 print(pokemon) """
 
-
 import requests
 
-def comic(input): 
+def words(input): 
     word = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{input}")
     dictionary = word.json()
-    print(dict(dictionary))
+    entry = dictionary[0]
+    definition = entry["meanings"][0]["definitions"][0]["definition"]
+    print(definition)
+words("pleb")
+
+
 
     
 
