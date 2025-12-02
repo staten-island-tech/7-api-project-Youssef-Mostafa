c371d941be382c98bfcb04eb5ab753d0feea5323
Youssef Mostafa <youssefm32@nycstudents.net>
2025-12-02 09:49:48 -0500

    Update API.py

--- API.py
+++ API.py
@@ -1,4 +1,4 @@
-
+# Examples
 
 """ def getPoke(poke):
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
@@ -17,7 +17,25 @@
 pokemon = getPoke("Bulbasaur")
 print(pokemon) """
 
+
+# API Project
+
+import tkinter as tk
 import requests
+
+
+window = tk.Tk()
+window.title("DEFINITION FINDER")
+window.geometry("1000x1000")
+window.resizable(False, False)
+input = tk.Label(window, text = "Write a word to search for a definition:")
+input.pack(pady = 10)
+
+
+
+
+
+window.mainloop()
 
 def words(input): 
     word = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{input}")
@@ -25,9 +43,8 @@ def words(input):
     entry = dictionary[0]
     definition = entry["meanings"][0]["definitions"][0]["definition"]
     print(definition)
-words("pleb")
+words("yell")
 
 
 
-    
 
